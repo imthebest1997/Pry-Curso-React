46a9a6ba95e30031e6bb38ff577d216f39024473
imthebest1997 <imthebest_1997@hotmail.com>
2023-09-28 19:05:06 -0500

    Projects updated

---
+++ 07-SPA-Heroes/src/main.jsx
@@ -0,0 +1,14 @@
+import "./styles.css"
+
+import { BrowserRouter } from "react-router-dom"
+import { HeroesApp } from './HeroesApp'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <HeroesApp />  
+    </BrowserRouter>
+  </React.StrictMode>,
+)
